0c8099daeb7a846652aa4e8afafe24ef84bb6b49
DuverMelo26 <damelo@ucundinamarca.edu.co>
2022-07-16 19:54:18 -0500

    Integracion de Menu desplegable

--- app/src/main/java/com/example/wildrunning/MainActivity.kt
+++ app/src/main/java/com/example/wildrunning/MainActivity.kt
@@ -3,23 +3,74 @@ package com.example.wildrunning
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.MenuItem
 import android.view.View
+import android.widget.TextView
 import android.widget.Toast
+import androidx.appcompat.app.ActionBarDrawerToggle
+import androidx.drawerlayout.widget.DrawerLayout
+import com.example.wildrunning.LoginActivity.Companion.providerSession
+import com.example.wildrunning.LoginActivity.Companion.userEmail
+import com.google.android.material.navigation.NavigationView
 import com.google.firebase.auth.FirebaseAuth
 import com.google.firebase.ktx.Firebase
 
-class MainActivity : AppCompatActivity() {
+class MainActivity : AppCompatActivity(), NavigationView.OnNavigationItemSelectedListener {
+
+    private lateinit var drawer: DrawerLayout
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        initToolBar()
+        initNavigationView()
+
     }
 
-    fun cerrarSesion(view: View){
+    private fun initToolBar() {
+        val toolbar: androidx.appcompat.widget.Toolbar = findViewById(R.id.toolbarMain)
+        setSupportActionBar(toolbar)
+
+        drawer = findViewById(R.id.drawer_layout)
+        val toogle = ActionBarDrawerToggle(
+            this, drawer, toolbar, R.string.navigation_drawer_open,
+            R.string.navigation_drawer_close
+        )
+
+        drawer.addDrawerListener(toogle)
+
+        toogle.syncState()
+    }
+
+    private fun initNavigationView(){
+
+        var navigationView: NavigationView = findViewById(R.id.nav_view)
+        navigationView.setNavigationItemSelectedListener (this)
+
+        var headerView: View = LayoutInflater.from(this).inflate(R.layout.nav_header_main, navigationView, false)
+        navigationView.removeHeaderView(headerView)
+        navigationView.addHeaderView(headerView)
+
+        var tvUser: TextView = headerView.findViewById(R.id.tvUser)
+        tvUser.text = userEmail
+    }
+
+    fun callSingOut(view: View){
         singOut()
     }
     private fun singOut(){
+        userEmail = ""
+
+        //if(providerSession == "Facebook") LoginManager.getInstance().logOut()
+
         FirebaseAuth.getInstance().signOut()
         Toast.makeText(this, "Se ha cerrado con exito su sesión", Toast.LENGTH_SHORT).show()
         startActivity(Intent(this, LoginActivity::class.java))
+    }
+
+    override fun onNavigationItemSelected(item: MenuItem): Boolean {
+        TODO("Not yet implemented")
     }
 }
